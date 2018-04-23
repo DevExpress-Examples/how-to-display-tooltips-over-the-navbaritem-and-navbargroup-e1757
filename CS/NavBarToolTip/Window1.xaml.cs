5e4c7d33bbdba9016054f414cedb8f38af61edb6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NavBarToolTip/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Wpf.NavBar;
+using System.Windows.Controls.Primitives;
+using DevExpress.Wpf.Core;
+
+namespace NavBarToolTip {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        private void ExplorerBarView_ItemSelected(object sender, DevExpress.Wpf.NavBar.NavBarItemSelectedEventArgs e) {
+
+        }
+
+        private void NavBarControl_MouseMove(object sender, MouseEventArgs e) {
+            NavBarControl nb = sender as NavBarControl;
+            tooltip.IsOpen = false;
+            Cursor = Cursors.Arrow;
+
+            NavBarItem nbItem = nb.View.GetNavBarItem(e);
+            NavBarGroup nbGroup = nb.View.GetNavBarGroup(e);
+            if (nbItem == null && nbGroup != null) {
+                tooltip_text.Text = string.Format("Header = {0}\n", nbGroup.Header);
+                tooltip.Placement = PlacementMode.Mouse;
+                tooltip.IsOpen = true;
+                Cursor = Cursors.Hand;
+            }
+            if (nbItem != null) {
+                tooltip_text.Text = string.Format("Name = {0}\n", nbItem.Content);
+                tooltip.Placement = PlacementMode.Mouse;
+                tooltip.IsOpen = true;
+                Cursor = Cursors.Hand;
+            }
+
+        }
+
+        private void NavBarControl_MouseLeave(object sender, MouseEventArgs e) {
+            tooltip.IsOpen = false;
+        }
+    }
+}
---
+++ VB/NavBarToolTip/Window1.xaml.vb
@@ -0,0 +1,59 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Wpf.NavBar
+Imports System.Windows.Controls.Primitives
+Imports DevExpress.Wpf.Core
+
+Namespace NavBarToolTip
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub ExplorerBarView_ItemSelected(ByVal sender As Object, ByVal e As DevExpress.Wpf.NavBar.NavBarItemSelectedEventArgs)
+
+		End Sub
+
+		Private Sub NavBarControl_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
+			Dim nb As NavBarControl = TryCast(sender, NavBarControl)
+			tooltip.IsOpen = False
+			Cursor = Cursors.Arrow
+
+			Dim nbItem As NavBarItem = nb.View.GetNavBarItem(e)
+			Dim nbGroup As NavBarGroup = nb.View.GetNavBarGroup(e)
+			If nbItem Is Nothing AndAlso nbGroup IsNot Nothing Then
+				tooltip_text.Text = String.Format("Header = {0}" & Constants.vbLf, nbGroup.Header)
+				tooltip.Placement = PlacementMode.Mouse
+				tooltip.IsOpen = True
+				Cursor = Cursors.Hand
+			End If
+			If nbItem IsNot Nothing Then
+				tooltip_text.Text = String.Format("Name = {0}" & Constants.vbLf, nbItem.Content)
+				tooltip.Placement = PlacementMode.Mouse
+				tooltip.IsOpen = True
+				Cursor = Cursors.Hand
+			End If
+
+		End Sub
+
+		Private Sub NavBarControl_MouseLeave(ByVal sender As Object, ByVal e As MouseEventArgs)
+			tooltip.IsOpen = False
+		End Sub
+	End Class
+End Namespace
